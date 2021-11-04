cdda4f233c05465d4feb24bbf5ceb798981c73cc
jdonic <donicjakub@gmail.com>
2021-11-04 22:09:13 +0100

    Formating code with flake8

--- app.py
+++ app.py
@@ -8,18 +8,19 @@ def flipping(bet, choice):
     if(bet > money):
         return 0
     result = (random.choice(["Heads", "Tails"]))
-    if( choice == result):
+    if (choice == result):
         return bet
     else:
-       return -bet
+        return -bet
+
 
 def cho_han(bet, choice):
     if bet > money:
         return 0
-    first_dice = randrange(1,7)
-    second_dice = randrange(1,7)
+    first_dice = randrange(1, 7)
+    second_dice = randrange(1, 7)
     sum = first_dice + second_dice
-    if(sum%2 == 0):
+    if(sum % 2 == 0):
         if(choice == 'even'):
             return bet
         else:
@@ -30,11 +31,12 @@ def cho_han(bet, choice):
         else:
             return -bet
 
+
 def card_game(bet):
     if bet > money:
         return 0
-    player_turn = randrange(1,14)
-    computer_turn = randrange(1,14)
+    player_turn = randrange(1, 14)
+    computer_turn = randrange(1, 14)
     if(player_turn > computer_turn):
         return bet
     elif(player_turn < computer_turn):
@@ -46,15 +48,15 @@ def card_game(bet):
 def roulette(bet, guess):
     if bet > money:
         return 0
-    roulette_number = randrange(1,37)
+    roulette_number = randrange(1, 37)
     if(type(guess) == str):
         if(guess == "Even"):
-            if(roulette_number%2==0):
+            if(roulette_number % 2 == 0):
                 return bet
             else:
                 return -bet
         else:
-            if(roulette_number%2 == 1):
+            if(roulette_number % 2 == 1):
                 return bet
             else:
                 return -bet
@@ -63,8 +65,6 @@ def roulette(bet, guess):
             return 35*bet
         else:
             return -bet
-    
-
 
 
 money += (flipping(10, 'Heads'))
